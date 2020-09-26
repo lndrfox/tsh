b0ec5ef76f835d47fe4a3bf088f793a225a7f732
Robin Arnoux <robinjarnoux@gmail.com>
2020-09-26 18:09:43 +0200

    starting to work on mkdir

---
+++ commandes/mkdir.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include "tar.h"
+
+
+// TO USE : ./cat fichiertar.tar fichier autrefichier ...
+
+int main(int argc, char *argv[]){
+
+
+	return 0;
+}
