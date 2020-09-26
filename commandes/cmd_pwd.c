d332e0eb16f029866c52fffcab175584efc96cfd
Natacha <natacha@natacha.guijarro64@orange.fr>
2020-09-26 18:48:37 +0200

    Amélioration de la commande pwd

--- commandes/cmd_pwd.c
+++ commandes/cmd_pwd.c
@@ -1,13 +1,13 @@
 #include <stdio.h>  // printf
 #include <stdlib.h> // exit
-#include <unistd.h> // getcwd
-#include <string.h> // strcat
+#include <unistd.h> // getcwd close read
+#include <string.h> // strtok
 #include <fcntl.h>  //open
 #include "tar.h"
 
 int main(int argc, char *argv[]) {
 	if(argc != 2) {
-		perror("Fichier entré invalide");
+		printf("Fichier entré invalide\nFormat : test.tar/nomfichier\n");
 		exit(-1);
 	}
 
@@ -18,24 +18,31 @@ int main(int argc, char *argv[]) {
 	// argv[1] : nom du tar et du fichier (test.tar/nomfichier)
 	// Verifier l'existance du fichier
 
+	// Pour l'instant on ne prends pas en compte les entrées 
+	// de plus de un '/' (ex: test.tar/../../..)
 	char* tar = strtok(argv[1], "/");
 	char* nomfichier = strtok(NULL, "");
-	
-	if(nomfichier ==  NULL) {
-		printf("Veuillez rentrez un fichier : exemple.tar/fichier\n");
-		exit(-1);
-	}
 
-	char tampon[512];
-	int n;
-	struct posix_header * p_hdr;
 	int fd = open(tar, O_RDONLY);
 
+	// Erreur d'ouverture du tar
 	if (fd<0) {
 		perror("Erreur lors de l'ouverture du tar");
 		exit(-1);
 	}
 	
+	// Si le tarball est uniquement rentre
+	if(fd>=0 && nomfichier ==  NULL) {
+		printf("Le tarball existe :\n%s/%s \n", chemin, tar); 
+		close(fd);
+		exit(0);
+	}
+
+	char tampon[512];
+	int n;
+	struct posix_header * p_hdr;
+	
+	// Verifie si le fichier existe sinon
 	while((n = read(fd, &tampon, 512)) > 0) {
 		p_hdr = (struct posix_header*)tampon;
 		if(strcmp(p_hdr -> name, nomfichier) == 0) {
