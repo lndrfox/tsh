7e8f1a762916a78ad5f162a860e4e384e61736fd
Robin Arnoux <robinjarnoux@gmail.com>
2020-10-04 12:15:15 +0200

    Commenting mkdir.c properly

--- commandes/mkdir.c
+++ commandes/mkdir.c
@@ -10,7 +10,7 @@
 #include "tar.h"
 
 
-// TO USE : ./cat fichiertar.tar fichier autrefichier ...
+// TO USE : ./mkdir fichiertar.tar directory otherdirectory ...
 
 int main(int argc, char *argv[]){
 
@@ -37,14 +37,13 @@ int main(int argc, char *argv[]){
 
 	struct posix_header hd;
 
-		// THIS LOOP ALLOWS US TO LOOK FOR THE HEADER CORRESPONDING TO THE FILE WE WANT TO
-		// FIND IN THE TAR
+		// WE LOOP ON EVERY DIRECTORY THAT WE NEED TO CREATE
 
 	for (int i=2; i<argc;i++){
 
 		if(sizeof(argv[i])>sizeof(char[100])){
 
-			printf("Errorn directory %s name is too long", argv[i]);
+			printf("Error directory %s name is too long\n", argv[i]);
 			exit(-1);
 
 		}
@@ -66,14 +65,14 @@ int main(int argc, char *argv[]){
 				return -1;
 			}
 
-			//IF WE REACHED THE END OF THE TAR WITHOUT FINDING THE GOOD HEADER
+			//IF WE REACHED THE END OF THE TAR WITHOUT FINDING THE HEADER THEN IT DOESNT EXIST AND WE CAN CREATE IT
 
 			if((hd.name[0]=='\0')){
 
 				break;
 			}
 
-			//IF WE FOUND THE RIGHT HEADER, WE GET OUT OF THE LOOP
+			//IF WE FOUND THE HEADER, IT ALREADY EXISTS AND WE HAVE AN ERROR
 
 			if(strcmp(hd.name,argv[i])==0){
 
@@ -88,55 +87,97 @@ int main(int argc, char *argv[]){
 			unsigned int size;
 			sscanf(hd.size, "%o",&size);
 
-			//OTHERWISE WE GET TO THE NEXT HEADER
+			//WE GET TO THE NEXT HEADER
 
 			lseek(fd,((size+ BLOCKSIZE - 1) >> BLOCKBITS)*BLOCKSIZE,SEEK_CUR);
 
 
 		}while(strcmp(hd.name,argv[i])!=0);
 
+		//CREATING THE HEADER FOR THE NEW DIRECTORY
+
 
 		struct posix_header dir;
+
+		//INIT STRUCT MEMORY
+
 		memset(&dir,0,BLOCKSIZE);
+
+		//  WE NEED TO ADD / AT THE END OF THE DIRECTORY NAME FOR IT TO BE VALID
 
 		char name_tmp[100];
 		sprintf(name_tmp, argv[i]);
 		char name_tmp2 [100]="/";
-
 		strcat(name_tmp, name_tmp2);
-
 		sprintf(dir.name,name_tmp);
+
+		//FILLING MODE 
+
+
 		sprintf(dir.mode,"0000700");
+
+		//SIZE IS 0
+
 		sprintf(dir.size,"%011o",0);
+
+
+		//FILLING MAGIC FIELD
+
+
 		sprintf(dir.magic,TMAGIC);
+
+		// LAST MODIFICATION DATE
+
 		long t_acc= time(NULL);
 		sprintf(dir.mtime ,"%ld", t_acc);
+
+		//DIRECTORY SO TYPE FLAG IS 5
+
 		dir.typeflag='5';
+
+		// VERSION
+
 		sprintf(dir.version,TVERSION);
+
+		//USER NAME
+
 		getlogin_r(dir.uname, sizeof(dir.uname));
+
+		//GETTING GROUP NAME
 
 		gid_t gid= getgid();
 		struct group * g= getgrgid(gid);
+
+		//ERROR MANAGMENT
 
 		if(g==NULL){
 			perror("Reading group ID");
 			exit(-1);
 		}
-		//memset(dir.prefix,0,sizeof(dir.prefix));
-		//memset(dir.junk,0, sizeof(dir.junk));
-
 		sprintf(dir.gname,g->gr_name);
+
+		//SETTING CHECKSUM ONCE ALL THE OTHER FIELDS ARE FILLED
+
 		set_checksum(&dir);
+		
+		// SINCE WE READ THE FIRST EMPTY BLOCK (TAR ENDS WITH 2 EMPTY BLOCKS) TO CHECK IF WE HAD READ ALL OF THE HEADERS
+		// WE NEED TO GO BACK ONE BLOCK
 
 		lseek(fd,-512, SEEK_CUR);
 
+		//WRITING THE NEW DIRECTORY AT THE END OF THE FILE
+
 		int rdd=write(fd,&dir,sizeof(dir));
+
+		//ERROR MANAGEMENT
 
 		if(rdd<BLOCKSIZE){
 
 			perror("Error writing in file");
 			exit(-1);
 		}
+
+		//WE HAD THE TWO MANDATORY EMPTY BLOCKS AT THE END OF THE TAR FILE
 
 		char buff [BLOCKSIZE];
 		memset(buff,0,BLOCKSIZE);
@@ -156,8 +197,8 @@ int main(int argc, char *argv[]){
 
 
 
-		//WE GO BACK TO THE BEGINNING OF THE TAR FILE TO MAKE SURE WE DONT MISS
-		// THE HEADER WE WILL LOOK FOR
+		//WE GO BACK TO THE BEGINNING OF THE TAR FILE TO MAKE SURE THAT WE DONT CREATE A DIRECTORY THAT ALREADY EXISTS 
+		// FOR THE NEXT ARGUMENT
 
 		lseek(fd,0,SEEK_SET);
 
