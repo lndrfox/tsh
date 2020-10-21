c1066f6af330a0ccfd946a830455c33c913fb252
Natacha <natacha@natacha.guijarro64@orange.fr>
2020-10-21 19:21:48 +0200

    rmdir fonctionnelle

--- commandes/rmdir.c
+++ commandes/rmdir.c
@@ -1,16 +1,21 @@
-#include <stdio.h>		// sscanf sprintf perror
-#include <sys/types.h>	// lseek
+#include <stdio.h>		// sscanf perror
+#include <sys/types.h>		// lseek ftruncate
 #include <fcntl.h>		// open
-#include <unistd.h>		// read close lseek write
+#include <unistd.h>		// read close lseek write ftruncate
 #include <string.h>		// strcat strcmp
 #include <stdlib.h>		// exit
 #include "tar.h"
 #include "print.h"
 
 
-// Format: ./mkdir fichiertar.tar directory otherdirectory ...
+// Format: ./rmdir fichiertar.tar directory otherdirectory ...
+// (Un repertoire doit avoir '/' a la fin)
 
 int main(int argc, char *argv[]){
+
+	// ======================================================================
+	// 	 			INITIALISATION
+	// ======================================================================
 
 	// Conditions des valeurs d'entrée
 	if (argc < 2){
@@ -21,103 +26,133 @@ int main(int argc, char *argv[]){
 	// Ouverture du tar
 	int fd = open(argv[1],O_RDWR);
 	if(fd < 0){
-		perror("\033[1;31mErreur lors de l'ouverture du tar\n");
+		perror("\033[1;31mErreur lors de l'ouverture du tar");
 		exit(-1);
 	}
 
 	struct posix_header * p_hdr;
 	char tampon[512];
 
-	// On parcours pour chaque repertoire
+	// ======================================================================
+	// 	 	    PARCOURS DU TAR POUR CHAQUE REPERTOIRE
+	// ======================================================================
+
 	for (int i=2; i<argc;i++){
 
-		char* rep = NULL;
-		off_t longueur = 0;
-		size_t supp = 0;
-		size_t dep = 0;
+		int valide = 0;			// 0: le repertoire ne peut pas etre supprime
+						// 1: le repertoire peut etre supprime
 
-		// On parcours le tar
+		char* rep = NULL;		// Nom du repertoire
+		unsigned int size;		// Taille du fichier
+		off_t longueur = 0;		// Somme de la taille des fichiers avant le repertoire
+		off_t supp = 0;			// Somme de la taille du repertoire et de ses fichiers
+		off_t dep = 0;			// Somme de la taille des fichiers apres le repertoire
+
+		// ----------------------------------------------------------------------
+		// 	 		       PARCOURS DU TAR
+		// ----------------------------------------------------------------------
+
 		while(1) {
 
 			// Lecture du bloc
+
 			int rdcount = read(fd,&tampon, BLOCKSIZE);
 			if(rdcount<0){
-				perror("\033[1;31mErreur lors de la lecture du tar\n");
+				perror("\033[1;31mErreur lors de la lecture du tar");
 				close(fd);
 				exit(-1);
 			}
+
 			// Extraction des informations
+
 			p_hdr = (struct posix_header*)tampon;
-			unsigned int size;
 			sscanf(p_hdr->size, "%o",&size);
 
 			// On arrive a la fin au bloc nul
+
 			if(strlen(p_hdr-> name) == 0) {
 				if (rep == NULL) {
-					prints("\033[1;31mLe répertoire n'existe pas\n");
-					exit(-1);
+					prints("\033[1;31mrmdir: impossible de supprimer '");
+					prints(argv[i]);
+					prints("': Aucun fichier ou dossier de ce type\033[0m\n");
+					break;
 				}
 				else {
-					dep = dep + 512;
-					break;
+					if(supp != BLOCKSIZE) {
+						prints("\033[1;31mrmdir: impossible de supprimer '");
+						prints(argv[i]);
+						prints("': Le dossier n'est pas vide\033[0m\n");
+						break;
+					}
+					else {
+						valide = 1;
+						dep = dep + BLOCKSIZE;
+						break;
+					}
 				}
 			}
 
 			// Si on trouve le repertoire
+
 			if(strcmp(p_hdr->name, argv[i]) == 0 && p_hdr->typeflag == '5') {
 				rep = malloc(strlen(p_hdr->name) + 1);
 				strcpy(rep, strtok(p_hdr->name,"/"));
 			}
 
 			// Stockage des octets a utiliser lors de la suppresion
+
 			if(rep != NULL) {
-				// Le repertoire et ses sous fichiers a supprimer
+				// Le repertoire et ses fichiers a supprimer
 				strtok(p_hdr-> name, "/");
 				if(strcmp(p_hdr-> name, rep) == 0) 
-					supp = supp + 512 + size;
+					supp = supp + BLOCKSIZE + (((size+ BLOCKSIZE - 1) >> BLOCKBITS)*BLOCKSIZE);
 
 				// Toutes donnees se situant apres le repertoire
 				else
-					dep = dep + 512 + size; 
+					dep = dep + BLOCKSIZE + (((size+ BLOCKSIZE - 1) >> BLOCKBITS)*BLOCKSIZE); 
 			}
 			// Toutes donnees avant d'avoir trouve le repertoire
 			else 
-				longueur = longueur + 512 + size;
+				longueur = longueur + BLOCKSIZE + (((size+ BLOCKSIZE - 1) >> BLOCKBITS)*BLOCKSIZE);
 
 			// On passe a l'entete suivante
+
 			lseek(fd,((size+ BLOCKSIZE - 1) >> BLOCKBITS)*BLOCKSIZE,SEEK_CUR);
 
 		}
 
-		// Traitement du repertoire
-		// (Le 1er bloc nul a ete lu dans la boucle)
+		// ----------------------------------------------------------------------
+		// 	 		  TRAITEMENT DU REPERTOIRE
+		// ----------------------------------------------------------------------
 
-		// On stocke les donnees a deplacer apres la suppression du repertoire
+		if (valide == 1) {
 
-		lseek(fd, -dep, SEEK_CUR);
-		dep = dep + 512;
-		char mem[dep];
+			// On stocke les donnees a deplacer apres la suppression du repertoire
+			// Rappel : le 1er bloc nul a ete lu dans la boucle
 
-		printd(longueur); prints(" : "); printd(supp); prints(" : "); printd(dep); prints("\n");
+			lseek(fd, -dep, SEEK_CUR);
+			dep = dep + BLOCKSIZE;
+			char mem[dep];
 
-		int rd = read(fd, &mem, dep);
-		if(rd<0){
-			perror("\033[1;31mErreur lors de la lecture du tar");
-			close(fd);
-			exit(-1);
+			int rd = read(fd, &mem, dep);
+			if(rd<0){
+				perror("\033[1;31mErreur lors de la lecture du tar");
+				close(fd);
+				exit(-1);
+			}
+
+			// On supprime le repertoire
+
+			lseek(fd, longueur, SEEK_SET);
+			int wr = write(fd, &mem, dep);
+			if(wr<0){
+				perror("\033[1;31mErreur lors de l'écriture du tar");
+				close(fd);
+				exit(-1);
+			}
+
+			ftruncate(fd, longueur+dep);
 		}
-
-		// On supprime le repertoire
-
-		lseek(fd, longueur, SEEK_SET);
-		int wr = write(fd, &mem, dep);
-		if(wr<0){
-			perror("\033[1;31mErreur lors de l'écriture du tar");
-			close(fd);
-			exit(-1);
-		}
-
-		ftruncate(fd, longueur+dep);
 
 		// Retour au depart
 
