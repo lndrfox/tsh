ee34d6ad0fb328e4d81d251df17c4e256b190886
Robin Arnoux <robinjarnoux@gmail.com>
2020-10-24 00:02:44 +0200

    Implementing cd for non tar path , fixing cat so that it works in the loop

--- commandes/cd.h
+++ commandes/cd.h
@@ -6,8 +6,26 @@
 
 void cd (char * p){
 
+	/*---IF WE HAVE NO ARGUMENT WE RETURN TO HOME---*/
 
-	if(current_dir_is_tar() || string_contains_tar(p)){
+	if(p==NULL){
+
+		char * log = getlogin();
+		char * home= malloc(strlen("/home/")+sizeof(char));
+		home = strcpy(home,"/home/");
+		home=realloc(home,strlen(home)+strlen(log)+sizeof(char));
+		home=strcat(home,log);
+
+		if(chdir(home)!=0){
+			perror("chdir");
+		}
+
+		return;
+	}
+
+	/*---IF WE ARE ALREADY IN A TAR OR WANT TO ACCESS A TAR----*/
+
+	else if(current_dir_is_tar() || string_contains_tar(p)){
 
 		/*BUILDING THE FULL TAR PATH, ACTUAL PATH IN TAR + ARG */
 
@@ -18,27 +36,83 @@ void cd (char * p){
 		path=realloc(path,sizeof(path)+strlen(p));
 		path=strcat(path,p);
 
-		/*CHECKING IF PATH IS VALID AND STORING IT*/
 
-		path = path_is_valid(path);
 
-		/*IF THE PATH IS NOT VALID*/
+		//CHECKING IF PATH IS VALID AND STORING IT
 
-		if(path==NULL){
+		//path = path_is_valid(path);
 
+		//IF THE PATH IS NOT VALID
+
+		/*if(path==NULL){
+
+
+		}*/
+
+
+		//IF THE PATH IS VALID WE UPDATE IT
+
+		//else{
+
+			setenv("tar","c.tar",1);
+		//}
+	}
+
+	/*-----IF WE ARE NOT DEALING WITH TAR FILES-----*/
+
+	else{
+
+
+		/*TOKING THE PATH*/
+
+		char ** tokens = decompose(p,"/");
+		int cpt=0;
+
+		/*-----ABSOLUTE PATH------*/
+
+		char c =p[0];
+
+		if(c == '/'){
+
+			char absolute [1+strlen(tokens[cpt])];
+			strcat(absolute,"/");
+			strcat(absolute,tokens[cpt]);
+			printf("%s\n",absolute);
+
+			/*ERROR MANAGMENT*/
+
+			if(chdir(absolute)!=0){
+
+				perror("chdir");
+			}
+
+			cpt++;
 
 		}
 
 
-		/*IF THE PATH IS VALID WE UPDATE IT*/
+		/*----RELATIVE PATH------*/
 
 		else{
 
-			setenv("tar",path,1);
+			cpt=0;
 		}
-	}
 
-	else{
+		/*WE ACCESS EACH TOKEN*/
+
+		while(tokens[cpt]!=NULL){
+
+			/*ERROR MANAGMENT*/
+
+			if(chdir(tokens[cpt])!=0){
+
+				perror("chdir");
+				
+			}
+
+			cpt++;
+
+		}
 
 
 	}
